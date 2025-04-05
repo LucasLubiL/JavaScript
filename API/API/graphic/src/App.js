4da81d100bc927bc82b81e62030400cb09510f6a
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-04-05 18:34:42 -0300

    Add files via upload

--- API/API/graphic/src/App.js
+++ API/API/graphic/src/App.js
@@ -1,9 +1,26 @@
-import Chart from './chart'
+import { useEffect, useState } from 'react';
+import Chart from './chart';
+import { getCandles } from './dataService';
 
 function App() {
+  
+  const [data, setData] = useState([]);
+  const [moeda, setMoeda] = useState('');
+
+  useEffect(() => {
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const moedaSelecionada = urlParams.get('moeda');
+
+    getCandles(moedaSelecionada, '1m')
+      .then(data => setData(data))
+      .catch(err => alert(err.response ? err.response.data : err.message));
+  }, []);
+
   return (
     <div>
-      <Chart/>
+      <h2>Gráfico: {moeda}</h2>
+      <Chart data={data} />
     </div>
   );
 }
