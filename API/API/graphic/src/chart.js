1d0dac45616b0cc2beceae47eb2eb4c95a24c8d7
Lucas Amaral <144184205+LucasLubiL@users.noreply.github.com>
2025-04-06 15:21:45 -0300

    Add files via upload

--- API/API/graphic/src/chart.js
+++ API/API/graphic/src/chart.js
@@ -4,15 +4,29 @@ export default function chart(props){
 
     const options = {
         xaxis: {
-            type: 'datetime'
-        },
+            type: 'datetime',
+            labels: {
+              datetimeUTC: false, // <- ESSENCIAL! Mostra a hora local
+              format: 'dd/MM HH:mm',
+              formatter: function (value, timestamp) {
+                return new Date(timestamp).toLocaleString('pt-BR', {
+                  timeZone: 'America/Sao_Paulo',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  day: '2-digit',
+                  month: '2-digit',
+                });
+              },
+            },
+          }, 
+    
         yaxis:{
             tooltip: {
                 enable: true
             }
         }
+    
     }
-
     const series =[{
         data: props.data
     
@@ -23,6 +37,6 @@ export default function chart(props){
                     series={series}
                     type="candlestick"
                     width={800}
-                    heigth={600}/>
+                    heigth={800}/>
     )
 }
